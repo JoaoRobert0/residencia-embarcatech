c33459aeaef6f5232b6aa2649cd40aea4787cd0b
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-04-25 19:04:33 -0300

    joystick controlando display o-led

--- sistemas-digitais/unidade-02/tarefa/portas-logicas/portas-logicas.c
+++ sistemas-digitais/unidade-02/tarefa/portas-logicas/portas-logicas.c
@@ -6,6 +6,7 @@
 #include "pico/binary_info.h"
 #include "inc/ssd1306.h"
 #include "hardware/i2c.h"
+#include "hardware/adc.h"
 
 const uint I2C_SDA = 14;
 const uint I2C_SCL = 15;
@@ -64,7 +65,7 @@ int main()
     stdio_init_all();   // Inicializa os tipos stdio padrão presentes ligados ao binário
 
     // Inicialização do i2c
-    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    i2c_init(i2c1, 700 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
@@ -73,15 +74,56 @@ int main()
     // Processo de inicialização completo do OLED SSD1306
     ssd1306_init();
 
-    while(true) {
-        escrever_porta_logica("nand");
-        sleep_ms(2000);
+    // Inicializa o conversor analógico-digital (ADC)
+    adc_init();
 
-        escrever_porta_logica("or");
-        sleep_ms(2000);
+    // Configura os pinos GPIO 26 e 27 como entradas de ADC (alta impedância, sem resistores pull-up)
+    adc_gpio_init(26);
+    adc_gpio_init(27);
 
-        escrever_porta_logica("and");
-        sleep_ms(2000);
+
+    uint8_t estado = 0;
+    while (1) {
+        // Seleciona a entrada ADC 1 (conectada ao eixo Y do joystick)
+        adc_select_input(0);
+        // Lê o valor do ADC para o eixo Y
+        uint adc_y_raw = adc_read();
+
+        if (adc_y_raw > 3000) {
+            // Avança para o próximo estado (ciclo circular)
+            estado = (estado + 1) % 7;
+        } else if (adc_y_raw < 1400) {
+            // Retrocede para o estado anterior (ciclo circular)
+            estado = (estado + 6) % 7;  // Adiciona 6 para evitar números negativos
+        }
+
+        printf("%u\n", adc_y_raw);
+        printf("%u\n\n", estado);
+
+        switch (estado) {
+            case 0:
+                escrever_porta_logica("AND");
+                break;
+            case 1:
+                escrever_porta_logica("OR");
+                break;
+            case 2:
+                escrever_porta_logica("NOT");
+                break;
+            case 3:
+                escrever_porta_logica("NAND");
+                break;
+            case 4:
+                escrever_porta_logica("NOR");
+                break;
+            case 5:
+                escrever_porta_logica("XOR");
+                break;
+            case 6:
+                escrever_porta_logica("XNOR");
+                break;
+        }
+        sleep_ms(200);
     }
 
     return 0;
